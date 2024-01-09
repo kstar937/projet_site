37295e5ccae89d2807aa3de820c10f79c9b89373
kstar937 <112125132+kstar937@users.noreply.github.com>
2024-01-09 20:48:59 +0100

    1

---
+++ footer.php
@@ -0,0 +1,116 @@
+<html>
+<body>
+
+<footer class="text-center text-lg-start bg-body-tertiary text-muted">
+  <section class="d-flex justify-content-center justify-content-lg-between p-4 border-bottom">
+    <div class="me-5 d-none d-lg-block">
+    </div>
+
+    <div>
+      <a href="" class="me-4 text-reset">
+        <i class="fab fa-facebook-f"></i>
+      </a>
+      <a href="" class="me-4 text-reset">
+        <i class="fab fa-twitter"></i>
+      </a>
+      <a href="" class="me-4 text-reset">
+        <i class="fab fa-google"></i>
+      </a>
+      <a href="" class="me-4 text-reset">
+        <i class="fab fa-instagram"></i>
+      </a>
+      <a href="" class="me-4 text-reset">
+        <i class="fab fa-linkedin"></i>
+      </a>
+      <a href="" class="me-4 text-reset">
+        <i class="fab fa-github"></i>
+      </a>
+    </div>
+  </section>
+
+
+  <section class="">
+    <div class="container text-center text-md-start mt-5">
+
+        <div class="row mt-3">
+
+         <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+
+            <h6 class="text-uppercase fw-bold mb-4">
+            <i class="fas fa-shop me-3"></i>K&S Store
+          </h6>
+          <p style=" text-align: justify; ">
+           Ici, vous allez retrouver touts les produits tels que des livres, produits High Tech (smartphone, ordinateur) ou encore des produits de maison
+          </p>
+        </div>
+
+        
+        <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+          <h6 class="text-uppercase fw-bold mb-4">
+            Produits
+          </h6>
+          <p>
+            <a href="#!" class="text-reset">Livres</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">High Tech</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">Electroménagers</a>
+          </p>
+        </div>
+
+
+
+        <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+          <h6 class="text-uppercase fw-bold mb-4">
+           Liens nécessaires
+          </h6>
+          <p>
+            <a href="#!" class="text-reset">A propos de nous</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">Panier</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">Aide</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">Vos informations personnelles</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">Cookies</a>
+          </p>
+        </div>
+
+        
+
+        <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+          <h6 class="text-uppercase fw-bold mb-4">Contact</h6>
+          <p><i class="fas fa-home me-3"></i>Classe de 1ère NSI</p>
+          <p>
+            <i class="fas fa-envelope me-3"></i>
+            K&S_store@gmail.com
+          </p>
+          <p><i class="fas fa-phone me-3"></i> 0033 6 22 77 52 55</p>
+        </div>
+      </div>
+    </div>
+  </section>
+
+
+  <div class="text-center p-4" style="background-color: rgba(0, 0, 0, 0.05);">© 2024 Copyright:
+    <a class="text-reset fw-bold" href="https://techparis.net/shop">Techparis.net/shop</a>
+  </div>
+  
+
+</footer>
+</body>
+</html>
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+
+<!-- MDB --> 
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/7.1.0/mdb.umd.min.js" ></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
---
+++ header.php
@@ -0,0 +1,51 @@
+<head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <!-- Font Awesome -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet"/>
+    <!-- Google Fonts -->
+    <link  href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"  rel="stylesheet"/>
+    <!-- MDB -->
+    <link  href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/7.1.0/mdb.min.css"  rel="stylesheet"/>
+</head>
+
+<html>
+
+<nav class="navbar navbar-expand-lg navbar-light bg-body-tertiary">
+
+<div class="container-fluid">
+
+  <button data-mdb-collapse-init class="navbar-toggler" type="button" data-mdb-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <i class="fas fa-bars"></i>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <a class="navbar-brand mt-2 mt-lg-0" href="index.php">
+        K&S Store
+      </a>
+
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link" href="#">Meilleur Ventes</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Livres</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">High Tech</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Electroménagers</a>
+        </li>
+      </ul>
+    </div>
+
+    <div class="d-flex align-items-center">
+      <a class="text-reset me-3" href="#">
+        <i class="fas fa-shopping-cart"></i>
+      </a>
+
+    </div>
+  </div>
+</nav>
+</html>
+
